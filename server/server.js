18257105edbf1e4e201b24379814bd64978af97f
Lakshay sharma <lakshay0328@gmail.com>
2025-07-18 12:21:11 +0530

    cors fixed

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const helmet = require('helmet');
+const helmet = require("helmet");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const userRoutes = require("./routes/userRouter");
@@ -9,13 +9,30 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const allowedOrigins = [
+  "https://finance-tracker-lemon-six.vercel.app",
+  "http://localhost:3000"
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"]
+  })
+);
+
 app.use(helmet());
-app.use(cors());
 app.use(express.json());
 
 app.use("/api/users", userRoutes);
 app.use("/api/transactions", transactionRoutes);
-
 
 const connectDB = require("./config/db");
 connectDB();
